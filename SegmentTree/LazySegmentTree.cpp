423cbca1a2c788db90fdf23b491b8c78de1cf494
Charly52830 <g.m.52830A@gmail.com>
2020-11-08 23:39:06 -0600

    Código de LazySegmentTree probado, corregido y documentado

--- SegmentTree/LazySegmentTree.cpp
+++ SegmentTree/LazySegmentTree.cpp
@@ -1,40 +1,47 @@
-#include <string.h>
 #include <vector>
 #include <iostream>
-#define L(k) (k << 1)
-#define R(k) (k << 1) + 1
 using namespace std;
 
 /**
- * Definición de Segment Tree para distintas preguntas comunes.
- * range minimum query: 
- *  SegmentTree<int> st(v, min_function);
+ * Definición de Segment Tree Lazy para preguntas y actualizaciones en rangos.
+ * range sum query + range assign update:
+ *  LazySegmentTree<int> st(v, sum_function, lazy_assign_sum);
  *
- * range maximum query:
- *  SegmentTree<int> st(v, max_function);
+ * range maximum/minimum query + range assign update:
+ *  LazySegmentTree<int> st(v, max_function, lazy_assign_max);
  *
- * range sum query:
- *  SegmentTree<int> st(v, sum_function);
+ * range sum query + range sum update:
+ *  LazySegmentTree<int> st(v, sum_function, lazy_sum_sum);
  *
- * range gcd/lcm query:
- *  SegmentTree<int> st(v, gcd_function);
+ * range maximum/minimum query + range sum update:
+ *  LazySegmentTree<int> st(v, max_function, lazy_sum_max);
  *
- * range sum query con n elementos iniciados en 0:
- *  SegmentTree<int> st(n, sum_function, 0);
- * 
- * actualizar arreglo sustituyendo el valor del índice i con el valor:
- *  st.update(i, val, false);
+ * Se puede formar cualquier otro Segment Tree ajustando adecuadamente las
+ * funciones.
  *
- * actualizar arreglo sumando el valor del índice i con el valor val:
- *  st.update(i, val, true);
+ * Para range update + single query puede utilizarse cualquier versión del
+ * LazySegmentTree, solo se debe considerar si la actualización en rangos es
+ * de asignación o de suma, y se puede utilizar st.query(i, i) para responder.
  *
- * Se puede formar cualquier otro Segment Tree con funciones específicas.
+ * Las funciones de range query deben regresar el tipo de dato del que se
+ * declaró el Segment Tree, y deben recibir 2 parámetros también del tipo que
+ * se declaró el Segment Tree. 
  *
- * Las funciones deben regresar el tipo de dato del que se declaró el Segment Tree.
- * Las funciones deben recibir 2 parámetros del tipo de dato del que se declaró
- * el Segment Tree.
+ * Las funciones de propagación deben recibir 6 parámetros y no es necesario
+ * utilizarlos todos:
+ *  tree: referencia a un nodo del Segment Tree
+ *  lazy: referencia al valor de propagación del nodo
+ *  marked: si es true entonces el nodo contiene información no propagada en
+ *   lazy, si es false entonces el nodo no tiene propagación pendiente
+ *  update_val: valor con el que se va a actualizar el nodo
+ *  l: índice inferior del segmento del arreglo que abarca el nodo
+ *  r: índice superior del segmento del arreglo que abarca el nodo
  */
 
+#define L(k) (k << 1)
+#define R(k) (k << 1) + 1
+
+// Funciones de range query
 int min_function(int a,int b) {
 	return min(a, b);
 }
@@ -51,6 +58,7 @@ int gcd_function(int a, int b) {
 	return !b ? a : gcd_function(b, a % b);
 }
 
+// Funciones de propagación
 void lazy_assign_sum(int & tree, int & lazy, bool marked, int update_val, int l, int r) {
 	tree = update_val * (r - l + 1);
 	lazy = update_val;
@@ -63,22 +71,25 @@ void lazy_assign_max(int & tree, int & lazy, bool marked, int update_val, int l,
 
 void lazy_sum_sum(int & tree, int & lazy, bool marked, int update_val, int l, int r) {
 	tree += update_val * (r - l + 1);
-	if(marked) lazy += update_val;
-	else lazy = update_val;
+	if(marked)
+		lazy += update_val;
+	else
+		lazy = update_val;
 }
 
 void lazy_sum_max(int & tree, int & lazy, bool marked, int update_val, int l, int r) {
 	tree += update_val;
-	if(marked) lazy += update_val;
-	else lazy = update_val;
+	if(marked)
+		lazy += update_val;
+	else
+		lazy = update_val;
 }
 
 template <typename T>
 class LazySegmentTree {
 	
 	/**
-	 * Segment tree que soporta operaciones de preguntas en rangos y update de un
-	 * solo valor.
+	 * Segment tree que soporta preguntas y actualizaciones en rangos.
 	 *
 	 * Construcción: O(n log n)
 	 * Actualización: O(log n)
@@ -86,9 +97,7 @@ class LazySegmentTree {
 	 * Memoria: O(12n)
 	 * n: tamaño del arreglo
 	 *
-	 * Funciona para responder fácilmente otro tipo de preguntas en O(log n) 
-	 * a partir de un Segment tree básico, por ejemplo:
-	 *
+	 * Otras preguntas en O(log n):
 	 * - Encontrar el k-ésimo cero en todo el arreglo
 	 * - Encontrar el prefijo de todo el arreglo cuya suma es >= x
 	 * - Encontrar el primer elemento mayor a x en un rango
@@ -97,8 +106,8 @@ class LazySegmentTree {
 	T (*func) (T,T);
 	void (*lazy_function) (T&,T&,bool,T,int,int);
 	T *tree, *lazy;
+	bool * marked;
 	int n;
-	bool * marked = NULL;
 	
 	/**
 	 * Operación para construir recursivamente el segment tree.
@@ -115,6 +124,9 @@ class LazySegmentTree {
 		}
 	}
 	
+	/**
+	 * Operación que realiza la propagación perezosa
+	 */
 	void push(int k, int l, int r, int m) {
 		if(marked[k]) {
 			lazy_function(tree[L(k)], lazy[L(k)], marked[L(k)], lazy[k], l, m);
@@ -126,7 +138,7 @@ class LazySegmentTree {
 	}
 
 	/**
-	 * Operación recursiva para responder la pregunta básica del segment tree.
+	 * Operación recursiva para responder la pregunta básica del segment tree
 	 */
 	T query(int i, int j, int k, int l, int r) {
 		if(i <= l && r <= j) return tree[k];
@@ -142,9 +154,12 @@ class LazySegmentTree {
 			return query(i, j, R(k), m + 1, r);
 	}
 	
+	/**
+	 * Operación recursiva para actualizar el segment tree en rangos
+	 */
 	void update(int i, int j, int k, int l, int r, T val) {
 		if(i <= l && r <= j)
-			lazy_function(tree[k], lazy[k], marked[k], val, l, r);
+			lazy_function(tree[k], lazy[k], marked[k], val, l, r), marked[k] = true;
 		else {
 			int m = l + r >> 1;
 			push(k, l, r, m);
@@ -159,10 +174,12 @@ class LazySegmentTree {
 	public:
 	
 	/**
-	 * Constructor que crea un Segment Tree a partir de un vector.
+	 * Constructor que crea un Segment Tree Lazy a partir de un vector.
 	 *
-	 * v: vector del que se construye el segment tree.
-	 * custom_function: función con la que trabaja el segment tree.
+	 * v: arreglo del que se construye el segment tree.
+	 * custom_function: función que define el tipo de pregunta en rangos a responder.
+	 * lazy_function: función de propagación que define cómo se lleva a cabo la 
+	 *  actualización en rangos de forma perezosa.
 	 */
 	LazySegmentTree(vector<T> & v, T (*custom_function) (T,T), void (*lazy_function) (T&,T&,bool,T,int,int)) {
 		func = custom_function;
@@ -175,12 +192,13 @@ class LazySegmentTree {
 	}
 	
 	/**
-	 * Constructor que crea un Segment Tree vacío, es decir, trabaja sobre un 
+	 * Constructor que crea un Segment Tree Lazy vacío, es decir, trabaja sobre un 
 	 * arreglo inexistente cuyos valores son puestos por default.
 	 *
 	 * size: tamaño del arreglo.
-	 * custom_function: función con la que trabaja el segment tree.
-	 * default_value: valor por defecto de los elementos del arreglo.
+	 * custom_function: función que define el tipo de pregunta en rangos a responder.
+	 * lazy_function: función de propagación que define cómo se lleva a cabo la 
+	 *  actualización en rangos de forma perezosa.
 	 */
 	LazySegmentTree(int size, T (*custom_function) (T,T), void (*lazy_function) (T&,T&,bool,T,int,int), T default_value = 0) {
 		vector<T> v(size, default_value);
@@ -192,21 +210,20 @@ class LazySegmentTree {
 	 * Indexado desde 0 y los segmentos son cerrados (la pregunta incluye a los
 	 * indices i y j).
 	 *
-	 * i: indice inferior.
-	 * j: indice superior.
+	 * i: indice inferior
+	 * j: indice superior
 	 */
 	T query(int i, int j) {
 		return query(i, j, 1, 0, n - 1);
 	}
 	
 	/**
-	 * Función para actualizar un solo elemento del arreglo.
+	 * Función para actualizar un segmento del arreglo.
 	 * Indexado desde 0.
 	 *
-	 * i: indice del arreglo a actualizar
-	 * val: valor con el que se va a actualizar el arreglo en la posición i
-	 * add: si add es false, el arreglo en la posición i se sustituirá con el nuevo
-	 *	valor, si es true, en arreglo en la posición i será sumado al valor actual.
+	 * i: indice inferior del segmento
+	 * j: indice superior del segmento
+	 * val: valor con el que se va a actualizar el segmento
 	 */
 	void update(int i, int j, T val) {
 		update(i, j, 1, 0, n - 1, val);
@@ -215,6 +232,6 @@ class LazySegmentTree {
 };
 
 int main() {
-	
+
 	return 0;
 }
